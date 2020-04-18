f243b5e4ba1edc85c6bb0db68e9fa0aabcf2998c
sjors <s.h.a.verschuren@student.tue.nl>
2020-04-18 17:49:58 +0200

    Completed OSC streaming + Fixed OSC stream + Added full data storage + Added separate storage frames for raw and fft

--- OSC_stream_tryout.py
+++ OSC_stream_tryout.py
@@ -4,77 +4,141 @@
 #
 # Author: S.H.A. Verschuren
 # Date:   17-4-2020
-# TODO: Implement proper data storage
+# TODO: Implement csv data storage
 
 import argparse
 import numpy as np
 import pandas as pd
 import datetime
+import time
 from pythonosc import dispatcher, osc_server
 
 
 def eeg_handler(unused_addr, args, ch1, ch2, ch3, ch4):
     # print("EEG per channel: ", ch1, ch2, ch3, ch4)
-    data['EEG_0'][data_id] = ch1
-    data['EEG_1'][data_id] = ch2
-    data['EEG_2'][data_id] = ch3
-    data['EEG_3'][data_id] = ch4
-    data_id += 1
-
-def alpha_handler(unused_addr, args, ch1, ch2, ch3, ch4, id):
-    print("Alpha amp per channel: ", ch1, ch2, ch3, ch4)
-    data['alpha_0'].append(ch1)
-    data['alpha_1'].append(ch2)
-    data['alpha_2'].append(ch3)
-    data['alpha_3'].append(ch4)
+    values = (ch1, ch2, ch3, ch4)
+    update_data('EEG', values)
+    write_data('raw')
 
 
-def beta_handler(unused_addr, args, ch1, ch2, ch3, ch4, id):
+def alpha_handler(unused_addr, args, ch1, ch2, ch3, ch4):
+    # print("Alpha amp per channel: ", ch1, ch2, ch3, ch4)
+    values = (ch1, ch2, ch3, ch4)
+    update_data('alpha', values)
+
+
+def beta_handler(unused_addr, args, ch1, ch2, ch3, ch4):
     # print("Beta amp per channel: ", ch1, ch2, ch3, ch4)
-    data['beta_0'].append(ch1)
-    data['beta_1'].append(ch2)
-    data['beta_2'].append(ch3)
-    data['beta_3'].append(ch4)
+    values = (ch1, ch2, ch3, ch4)
+    update_data('beta', values)
 
 
-def gamma_handler(unused_addr, args, ch1, ch2, ch3, ch4, id):
+def gamma_handler(unused_addr, args, ch1, ch2, ch3, ch4):
     # print("Gamma amp per channel: ", ch1, ch2, ch3, ch4)
-    data['gamma_0'].append(ch1)
-    data['gamma_1'].append(ch2)
-    data['gamma_2'].append(ch3)
-    data['gamma_3'].append(ch4)
+    values = (ch1, ch2, ch3, ch4)
+    update_data('gamma', values)
 
 
-def delta_handler(unused_addr, args, ch1, ch2, ch3, ch4, id):
+def delta_handler(unused_addr, args, ch1, ch2, ch3, ch4):
     # print("Delta amp per channel: ", ch1, ch2, ch3, ch4)
-    data['delta_0'].append(ch1)
-    data['delta_1'].append(ch2)
-    data['delta_2'].append(ch3)
-    data['delta_3'].append(ch4)
+    values = (ch1, ch2, ch3, ch4)
+    update_data('delta', values)
 
 
-def theta_handler(unused_addr, args, ch1, ch2, ch3, ch4, id):
+def theta_handler(unused_addr, args, ch1, ch2, ch3, ch4):
     # print("Theta amp per channel: ", ch1, ch2, ch3, ch4)
-    data['theta_0'].append(ch1)
-    data['theta_1'].append(ch2)
-    data['theta_2'].append(ch3)
-    data['theta_3'].append(ch4)
+    values = (ch1, ch2, ch3, ch4)
+    update_data('theta', values)
+    write_data('fft')
 
 
-def jaw_handler(unused_addr, args, ch1):
-    print("Jaw clench? : ", ch1)
+# def jaw_handler(unused_addr, args, ch1):
+#     # print("Jaw clench? : ", ch1)
 
 
-data = pd.DataFrame()
+def update_data(data_type="", values=()):
 
-columns = ["EEG_0", "EEG_1", "EEG_2", "EEG_3", "alpha_0", "alpha_1", "alpha_2", "alpha_3", "beta_0", "beta_1", "beta_2",
-           "beta_3", "gamma_0", "gamma_1", "gamma_2", "gamma_3", "delta_0", "delta_1", "delta_2", "delta_3", "theta_0",
-           "theta_1", "theta_2", "theta_3"]
+    if data_type == "EEG":
+        global eeg_data
+        eeg_data = values
+        # print(eeg_data)
+    elif data_type == "alpha":
+        global alpha_data
+        alpha_data = values
+    elif data_type == "beta":
+        global beta_data
+        beta_data = values
+    elif data_type == "gamma":
+        global gamma_data
+        gamma_data = values
+    elif data_type == "delta":
+        global delta_data
+        delta_data = values
+    elif data_type == "theta":
+        global theta_data
+        theta_data = values
+    else:
+        raise ValueError('Wrong data type ...')
 
-for column in columns:
-    data[column] = ""
+    # print('data updated: ', data_type, '=', values)
 
-data_id = 0
+
+def write_data(frame_type=''):
+    global raw_data
+    global fft_data
+
+    append_row = [time.perf_counter()]
+
+    if frame_type == 'raw':
+        data_point = eeg_data
+        for channel in range(4):
+            append_row.append(data_point[channel])
+
+        append_df = pd.DataFrame([append_row], columns=raw_columns)
+        raw_data = pd.concat([raw_data, append_df], ignore_index=True)
+        print("RAW: ", raw_data.shape)
+
+    elif frame_type == 'fft':
+        for data_type in ["alpha", "beta", "gamma", "delta", "theta"]:
+            if data_type == "alpha":
+                data_point = alpha_data
+            elif data_type == "beta":
+                data_point = beta_data
+            elif data_type == "gamma":
+                data_point = gamma_data
+            elif data_type == "delta":
+                data_point = delta_data
+            elif data_type == "theta":
+                data_point = theta_data
+            else:
+                raise ValueError('Wrong data type ...')
+
+            for channel in range(4):
+                append_row.append(data_point[channel])
+
+        append_df = pd.DataFrame([append_row], columns=fft_columns)
+        fft_data = pd.concat([fft_data, append_df], ignore_index=True)
+        print("FFT: ", fft_data.shape)
+    else:
+        raise ValueError('Wrong frame type ...')
+
+
+raw_columns = ["TimeStamp", "EEG_0", "EEG_1", "EEG_2", "EEG_3"]
+
+fft_columns = ["TimeStamp", "alpha_0", "alpha_1", "alpha_2", "alpha_3", "beta_0",
+           "beta_1", "beta_2", "beta_3", "gamma_0", "gamma_1", "gamma_2", "gamma_3", "delta_0", "delta_1", "delta_2",
+           "delta_3", "theta_0", "theta_1", "theta_2", "theta_3"]
+
+raw_data = pd.DataFrame(columns=raw_columns)
+fft_data = pd.DataFrame(columns=fft_columns)
+
+eeg_data = (0,0,0,0)
+alpha_data = (0,0,0,0)
+beta_data = (0,0,0,0)
+gamma_data = (0,0,0,0)
+delta_data = (0,0,0,0)
+theta_data = (0,0,0,0)
+
 
 if __name__ == '__main__':
     print('======== INIT OSC STREAM ========')
@@ -89,11 +153,11 @@ if __name__ == '__main__':
     dispatcher.map("/debug", print)
     dispatcher.map("/muse/eeg", eeg_handler, "EEG")
     # dispatcher.map("/muse/elements/jaw_clench", jaw_handler, "JAW")
-    # dispatcher.map("/muse/elements/alpha_absolute", alpha_handler, "Alpha")
-    # dispatcher.map("/muse/elements/beta_absolute", beta_handler, "Beta")
-    # dispatcher.map("/muse/elements/gamma_absolute", gamma_handler, "Gamma")
-    # dispatcher.map("/muse/elements/delta_absolute", delta_handler, "Delta")
-    # dispatcher.map("/muse/elements/theta_absolute", theta_handler, "Theta")
+    dispatcher.map("/muse/elements/alpha_absolute", alpha_handler, "Alpha")
+    dispatcher.map("/muse/elements/beta_absolute", beta_handler, "Beta")
+    dispatcher.map("/muse/elements/gamma_absolute", gamma_handler, "Gamma")
+    dispatcher.map("/muse/elements/delta_absolute", delta_handler, "Delta")
+    dispatcher.map("/muse/elements/theta_absolute", theta_handler, "Theta")
 
     server = osc_server.ThreadingOSCUDPServer((args.ip, args.port), dispatcher)
     print('\nListening at ', ip_address, ' {', Port, '}')
