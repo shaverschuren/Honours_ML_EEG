79332d5b789085b986004e859e5b713651048427
sjors <s.h.a.verschuren@student.tue.nl>
2020-05-21 16:28:48 +0200

    Improved quality check plot for use in report.

--- model/plot_model_performance.py
+++ model/plot_model_performance.py
@@ -20,25 +20,9 @@ X_train, X_val, y_train, y_val, X_test, y_test = train_models.load_dataset()
 
 # model = train_models.get_model()
 
-model = Sequential()
-model.add(InputLayer(input_shape=(20,)))
-model.add(Dense(25, activation='relu'))
-model.add(Dropout(0.05))
-model.add(Dense(50, activation='relu'))
-model.add(Dropout(0.05))
-model.add(Dense(50, activation='relu'))
-model.add(Dropout(0.05))
-model.add(Dense(75, activation='relu'))
-model.add(Dropout(0.05))
-model.add(Dense(50, activation='relu'))
-model.add(Dropout(0.05))
-model.add(Dense(25, activation='relu'))
-model.add(Dropout(0.05))
-model.add(Dense(10, activation='relu'))
-model.add(Dropout(0.05))
-model.add(Dense(1, activation='linear'))
+model = train_models.get_model()
 
-model.load_weights("logs\\tryout_logs\\model_20000iter_adadelta_float_y_nosigm\\20200520-015141\\model_20000iter_adadelta_float_y_nosigm_weights.hdf5")
+model.load_weights("logs\\merge_models\\merge_model_0\\merge_model_0_weights.hdf5")
 model.compile(loss='MSE', optimizer='adadelta', metrics=['MAE'])
 
 score = model.evaluate(X_test, y_test, verbose=0)
@@ -48,12 +32,27 @@ print("MAE: ", score[1])
 predictions = model.predict(X_test, verbose=0)
 
 df = pd.DataFrame()
-df['predictions'] = list(predictions)
-df['labels'] = y_test
+df['Predictions'] = list(predictions)
+df['Labels'] = y_test
+
+plt.figure()
 
 # plt.scatter(predictions, y_test)
-plot = sns.jointplot(x="predictions", y="labels", data=df)
-plot.ax_marg_x.set_xlim(0, 1)
-plot.ax_marg_y.set_ylim(0, 1)
+ax = sns.jointplot(y="Predictions", x="Labels", data=df, xlim=[0, 1], ylim=[0, 1], color='blue', s=5, alpha=0.2)
+# plot.ax_marg_x.set_xlim(0, 1)
+# plot.ax_marg_y.set_ylim(0, 1)
+# sns.lineplot([0, 1], [0, 100], color="red")
+# plt.suptitle('Joint scatter/density plot for predicted data')
+sns.lineplot([0, 1], [0, 1], color="red", ax=ax.ax_joint)
+sns.lineplot([0, 1], [0+score[1], 1+score[1]], color="firebrick", ax=ax.ax_joint)
+sns.lineplot([0, 1], [0+2*score[1], 1+2*score[1]], color="firebrick", ax=ax.ax_joint)
+sns.lineplot([0, 1], [0-score[1], 1-score[1]], color="firebrick", ax=ax.ax_joint)
+sns.lineplot([0, 1], [0-2*score[1], 1-2*score[1]], color="firebrick", ax=ax.ax_joint)
+for i in [1,3]:
+    ax.ax_joint.lines[i].set_linestyle("dashed")
+for i in [2,4]:
+    ax.ax_joint.lines[i].set_linestyle("dotted")
+
+ax.ax_joint.legend(['Reference', '68% CI', '95% CI'])
 
 plt.show()
